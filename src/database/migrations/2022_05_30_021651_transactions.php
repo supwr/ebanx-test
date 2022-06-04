1c096ca7cd59b0b173beb3beb6f9c310316c81cf
Marcelo Rodrigues <marcelo.rodriguespires@gmail.com>
2022-06-03 21:28:12 -0300

    Recovering migration files

---
+++ src/database/migrations/2022_05_30_021651_transactions.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('type');
+            $table->unsignedInteger('origin')->nullable();
+            $table->unsignedInteger('destination')->nullable();
+            $table->float('amount')->default(0);
+            $table->index('origin');
+            $table->index('destination');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
